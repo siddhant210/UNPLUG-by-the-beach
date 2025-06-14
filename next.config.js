a43de84e4543c2743a238972581ef303a31a8917
Siddhant Jadhav <157637908+siddhant210@users.noreply.github.com>
2025-06-14 17:02:50 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "./src/env.js";
+
+/** @type {import("next").NextConfig} */
+const config = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    webpackBuildWorker: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
+  },
+};
+
+export default config;
